267b5edb23870d383a1c80e961aabed821910b17
PhABC <ph.castonguay@gmail.com>
2018-09-08 12:03:29 -0400

    _init_ for class

--- services/classes.py
+++ services/classes.py
@@ -1,8 +1,19 @@
-class SignedTransferTransaction(object):
-    senderPubKey = []   # [x_key, y_key] of sender  
-    receiverPubKey = [] # [x_key, y_key] of receiver
-    tokenID = 0         # ID of token to transfer
-    R = []              # [R_x, R_y] R points of EDDCSA signature
-    S = 0               # S variable from EDDCSA signature
 
+# SignedTransferTransaction object that user sends to operator aggregating orders 
+#
+class SignedTransferTransaction(object):
     
+    ####
+    # senderPubKey = []   := [x_key, y_key] of sender  
+    # receiverPubKey = [] := [x_key, y_key] of receiver
+    # tokenID = 0         := ID of token to transfer
+    # R = []              := [R_x, R_y] R points of EDDCSA signature
+    # S = 0               := S variable from EDDCSA signature
+    ####
+    
+    def __init__(self, senderPubKey, receiverPubKey, tokenID, R, S):
+      self.senderPubKey = senderPubKey
+      self.receiverPubKey = receiverPubKey
+      self.tokenID = tokenID
+      self.R = R
+      self.S = S
