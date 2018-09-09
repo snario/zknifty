8173000dedc26540bd4446db0bc795516ce6a394
PhABC <ph.castonguay@gmail.com>
2018-09-09 02:36:09 -0400

    Fix initial merkle root generator

--- services/wallet/generate_initial_root.py
+++ services/wallet/generate_initial_root.py
@@ -6,23 +6,22 @@ from SparseMerkleTree import SparseMerkleTree
 
 import os
 
-bob_pk_x = os.getenv('BOB_PK_X')
-bob_pk_y = os.getenv('BOB_PK_Y')
+bob_sk = os.getenv('BOB_SK')
+alice_sk = os.getenv('ALICE_SK')
 
-alice_pk_x = os.getenv('ALICE_PK_X')
-alice_pk_y = os.getenv('ALICE_PK_Y')
+bobWallet = Wallet(sk= bob_sk)
+aliceWallet = Wallet(sk= alice_sk)
 
 # tokens = [ w.create_rhs_leaf(i) for i in range(4) ]
 leaves = {}
 
 # token 0 & 1 to Bob
-leaves[0] = w.create_leaf([bob_pk_x, bob_pk_y], str(0))
-leaves[1] = w.create_leaf([bob_pk_x, bob_pk_y], str(1))
+leaves[0] = bobWallet.create_leaf(bobWallet.public_key, str(0))
+leaves[1] = bobWallet.create_leaf(bobWallet.public_key, str(1))
 
 # token 2 & 3 to Alice
-leaves[2] = w.create_leaf([alice_pk_x, alice_pk_y], str(2))
-leaves[3] = w.create_leaf([alice_pk_x, alice_pk_y], str(3))
-
+leaves[2] = aliceWallet.create_leaf(aliceWallet.public_key, str(2))
+leaves[3] = aliceWallet.create_leaf(aliceWallet.public_key, str(3))
 
 print(leaves)
 tree = SparseMerkleTree(2, leaves)
