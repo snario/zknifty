33211250828b84f0c08de14e12ad2d91f7e042e8
Yondon Fu <yondon.fu@gmail.com>
2018-09-09 09:45:27 +0200

    Signer API: Enable CORS

--- services/wallet/signer/__init__.py
+++ services/wallet/signer/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(is_unit_test=False, identity='bob'):
     app = Flask(__name__)
+    CORS(app)
 
     if not is_unit_test:
         from dependency_config import container
